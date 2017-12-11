2baee7751220d0b45029619f48d045e1d4c035e6
Victor Manuel Tenorio Gomez <vm.tenorio@alumnos.urjc.es>
2017-12-11 12:47:12 +0100

    Cambiada tabulacion

--- src/main/java/urjc/isi/pruebasMockito/Main.java
+++ src/main/java/urjc/isi/pruebasMockito/Main.java
@@ -12,27 +12,27 @@ import java.sql.Statement;
 import java.net.URISyntaxException;
 
 public class Main {
-  
-
 
   public static String doWork() throws ClassNotFoundException, URISyntaxException {
-    String result = new String("Hola Mundo");
+	  String result = new String("Hola Mundo");
 
-    return result;
-    }
+	  return result;
+  }
 
   public static void main(String[] args) throws ClassNotFoundException {
-        port(getHerokuAssignedPort());
+	  port(getHerokuAssignedPort());
 
-        // spark server
-        get("/hello", (req, res) -> doWork());
+	  // spark server
+	  get("/hello", (req, res) -> doWork());
 
-        }
+  }
+  
   static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-    }
+	  ProcessBuilder processBuilder = new ProcessBuilder();
+	  if (processBuilder.environment().get("PORT") != null) {
+		  return Integer.parseInt(processBuilder.environment().get("PORT"));
+	  }
+	  return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+  }
+  
 }
